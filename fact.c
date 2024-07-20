5c57cc86e21b17f97ae643ea0e1d12e87bd34f87
murali <murali143009@gmail.com>
2024-07-20 04:46:41 +0000

    adding fact and add int

---
+++ fact.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Function to calculate factorial
+unsigned long long factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("Factorial of %d is %llu\n", number, factorial(number));
+    }
+
+    return 0;
+}
+
